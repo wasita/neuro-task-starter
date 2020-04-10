123024e38e53b45cc127be961abe4a1dc00bc423
wasita mahaphanit <wasita@brown.edu>
2020-04-10 12:20:56 -0400

    Revert "feat: install firebase; publish master branch for gh page"
    This reverts commit b07753858d27b6692c01129824eec61a75f13ff7.

--- src/lib/markup/photodiode.js
+++ src/lib/markup/photodiode.js
@@ -1,50 +1,53 @@
-import { AT_HOME, IS_ELECTRON } from "../../config/main";
-import { eventCodes } from "../../config/trigger";
-import $ from "jquery";
+import { AT_HOME, IS_ELECTRON } from  '../../config/main'
+import { eventCodes } from '../../config/trigger'
+import $ from 'jquery'
 
 // conditionally load electron and psiturk based on MTURK config variable
 let ipcRenderer = false;
 if (IS_ELECTRON) {
-  const electron = window.require("electron");
-  ipcRenderer = electron.ipcRenderer;
+  const electron = window.require('electron');
+  ipcRenderer  = electron.ipcRenderer;
 }
 
 // Relies on styling in App.css, generate PD spot
 const photodiodeGhostBox = () => {
-  const class_ = AT_HOME || !IS_ELECTRON ? "invisible" : "visible";
+	const class_ = (AT_HOME || !IS_ELECTRON) ? 'invisible' : 'visible'
 
   const markup = `<div class="photodiode-box ${class_}" id="photodiode-box">
 									<span id="photodiode-spot" class="photodiode-spot"></span>
-  								</div>`;
-  return markup;
-};
+  								</div>`
+	return markup
+}
 
 const pdSpotEncode = (taskCode) => {
   function pulse_for(ms, callback) {
-    $(".photodiode-spot").css({ "background-color": "black" });
-    setTimeout(() => {
-      $(".photodiode-spot").css({ "background-color": "white" });
-      callback();
-    }, ms);
-  }
-
-  function repeat_pulse_for(ms, i) {
-    if (i > 0) {
-      pulse_for(ms, () => {
-        setTimeout(() => {
-          repeat_pulse_for(ms, i - 1);
-        }, ms);
-      });
+      $('.photodiode-spot').css({"background-color": "black"})
+      setTimeout(() => {
+        $('.photodiode-spot').css({"background-color": "white"})
+        callback()
+      }, ms)
     }
-  }
 
-  if (!AT_HOME && IS_ELECTRON) {
-    const blinkTime = 40;
-    let numBlinks = taskCode;
-    if (taskCode < eventCodes.open_task) numBlinks = 1;
-    repeat_pulse_for(blinkTime, numBlinks);
-    if (ipcRenderer) ipcRenderer.send("trigger", taskCode);
-  }
-};
+    function repeat_pulse_for(ms, i) {
+      if (i > 0) {
+        pulse_for(ms, () => {
+          setTimeout(() => {
+            repeat_pulse_for(ms, i-1)
+          }, ms)
+        })
+      }
+    }
 
-export { photodiodeGhostBox, pdSpotEncode };
+		if (!AT_HOME && IS_ELECTRON) {
+				const blinkTime = 40
+				let numBlinks = taskCode
+		    if (taskCode < eventCodes.open_task) numBlinks = 1;
+				repeat_pulse_for(blinkTime, numBlinks)
+				if ( ipcRenderer ) ipcRenderer.send('trigger', taskCode)
+			}
+		}
+
+export {
+	photodiodeGhostBox,
+	pdSpotEncode
+}
